74d5b94fc137409dd64f7f8d4169e49147ff04b9
Mike Rosseel <mike.rosseel@gmail.com>
2022-11-18 21:38:44 +0100

    Added Dockerfile and installation script.
    The installation script can be used seperately but is to be used with
    raspbian bullseye

---
+++ pi-install.sh
@@ -0,0 +1,27 @@
+#/bin/sh
+sudo apt update && sudo apt upgrade
+sudo apt install -y netatalk
+sudo echo '[Homes]' >> /etc/netatalk/afp.conf 
+sudo echo 'basedir regex = /home' >> /etc/netatalk/afp.conf  
+sudo systemctl restart netatalk
+sudo apt install python-dev gcc cargo rustc libssl-dev # needed for poetry install
+sudo apt-get install -y libatlas-base-dev python3-dev # needed for astropy compile
+sudo apt install -y make automake gcc g++
+sudo apt install -y git pip neovim
+sudo apt install -y libcairo2-dev libnetpbm10-dev netpbm libpng-dev libjpeg-dev zlib1g-dev libbz2-dev swig libcfitsio-dev 
+cd /home/efinder 
+git clone https://github.com/dstndstn/astrometry.net.git
+echo 'export PATH=/home/efinder/.local/bin:$PATH' >> /etc/profile
+source /etc/profile
+#curl -sSL https://install.python-poetry.org | python3 -
+sudo pip install poetry
+cd /home/efinder/astrometry.net
+make
+make py
+make extra
+sudo make install
+export PATH=$PATH:/usr/local/astrometry/bin
+cd ../eFinder
+poetry install
+poetry shell
+python eFinderVNCGui.py -fn -fh
